db11e030d7da9b2c78c67acacc59c2faa05c8bc0
Tubin Shaji <tubin.shaji@gmail.com>
2019-10-08 11:56:13 +0100

    FizzBuzz program (#1)
    Add Java code for FizzBuzz problem

---
+++ FizzBuzz/src/com/tubz/FizzBuzz.java
@@ -0,0 +1,24 @@
+package com.tubz;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+
+		printFizzBuzz(100);
+	}
+
+	private static void printFizzBuzz(int limit) {
+		for (int j = 1; j <= 100; j++) {
+			if ((j % 3 == 0) && (j % 5 == 0)) {
+				System.out.println("FizzBuzz");
+			} else if (j % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (j % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(j);
+			}
+		}
+	}
+
+}

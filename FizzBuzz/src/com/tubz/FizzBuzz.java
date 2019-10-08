43ebbbfb3be582d378556cb88ac564fa353682f9
tubins <tubin.shaji@gmail.com>
2019-10-08 11:38:19 +0100

    Add function printFizzBuzz

--- FizzBuzz/src/com/tubz/FizzBuzz.java
+++ FizzBuzz/src/com/tubz/FizzBuzz.java
@@ -3,8 +3,22 @@ package com.tubz;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		printFizzBuzz(100);
+	}
+
+	private static void printFizzBuzz(int i) {
+		for (int j = 1; j <= 100; j++) {
+			if ((j % 3 == 0) && (i % 5 == 0)) {
+				System.out.println("FizzBuzz");
+			} else if (j % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (j % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(j);
+			}
+		}
 	}
 
 }
